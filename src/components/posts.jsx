b6919ef522985e73eb8864712abfc843e1a883c6
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-03-10 21:34:35 +0100

    updated query

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = ({ match }) => {
-  console.log(match.params);
+const Posts = ({ match, location }) => {
+  const { sortBy } = queryString.parse(location.search);
+  console.log(sortBy);
+
   return (
     <div>
       <h1>Posts</h1>
